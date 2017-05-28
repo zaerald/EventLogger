2d3afe515afea2270a4a32070271e0be344aadd1
zd-zero <zaerald.zd@gmail.com>
2017-05-29 02:11:28 +0800

    Add EventArrayAdapter Class

--- app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
+++ app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
@@ -1,13 +1,22 @@
 package zero.zd.daily_event_logger;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -42,5 +50,61 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class EventArrayAdapater extends ArrayAdapter<Event> {
+
+        private Context mContext;
+        private int mResource;
+        private List<Event> mEventList;
+
+        public EventArrayAdapater(Context context, int resource, List<Event> eventList) {
+            super(context, resource, eventList);
+            mContext = context;
+            mResource = resource;
+            mEventList = eventList;
+        }
+
+        @Override
+        public int getCount() {
+            return mEventList.size();
+        }
+
+        @Nullable
+        @Override
+        public Event getItem(int position) {
+            return mEventList.get(position);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            ViewHolder viewHolder;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(mResource, parent, false);
+
+                viewHolder = new ViewHolder();
+                viewHolder.eventTextView = (TextView) findViewById(R.id.text_event);
+                viewHolder.dateTextView = (TextView) findViewById(R.id.text_date);
+
+                convertView.setTag(viewHolder);
+            } else viewHolder = (ViewHolder) convertView.getTag();
+
+            Event event = mEventList.get(position);
+
+            viewHolder.eventTextView.setText(event.getEvent());
+            viewHolder.dateTextView.setText(event.getDate().toString());
+
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        private TextView eventTextView;
+        private TextView dateTextView;
     }
 }
