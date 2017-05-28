dc7d05509d348287767d998f0517fd9c3aeb0c25
zd-zero <zaerald.zd@gmail.com>
2017-05-29 01:27:43 +0800

    Refactor Text
    Change App Name
    
    Change Menu Settings to About

--- app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
+++ app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
@@ -37,16 +37,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_about:
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
