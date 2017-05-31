f5ab81b53b5d1fbdfd1efaea8c9837525120afdc
zd-zero <zaerald.zd@gmail.com>
2017-05-31 19:31:30 +0800

    Custom Dialog

--- app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
+++ app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
@@ -1,22 +1,25 @@
 package zero.zd.daily_event_logger;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -35,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showCreateEventDialog();
             }
         });
 
@@ -57,11 +59,44 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.action_about:
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showCreateEventDialog() {
+        ViewGroup dialogRootView = (ViewGroup) findViewById(R.id.root_dialog_create_event);
+        final View dialogView = getLayoutInflater()
+                .inflate(R.layout.dialog_create_event, dialogRootView);
+        final EditText eventEditText = (EditText) dialogView.findViewById(R.id.edit_event);
+
+        AlertDialog createEventDialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.title_event)
+                .setView(dialogView)
+                .setCancelable(false)
+                .setNegativeButton(R.string.action_discard, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton(R.string.action_save, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String test = eventEditText.getText().toString();
+                        Toast.makeText(MainActivity.this, "TEXT: " + test, Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        createEventDialog.show();
+    }
+
+    private static class ViewHolder {
+        private TextView eventTextView;
+        private TextView dateTextView;
     }
 
     private class EventArrayAdapter extends ArrayAdapter<Event> {
@@ -114,10 +149,5 @@ public class MainActivity extends AppCompatActivity {
 
             return convertView;
         }
-    }
-
-    private static class ViewHolder {
-        private TextView eventTextView;
-        private TextView dateTextView;
     }
 }
