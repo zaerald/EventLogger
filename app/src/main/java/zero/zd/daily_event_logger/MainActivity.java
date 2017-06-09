513e31d5e0862ef69b18745ce7ba4afa5e320388
zd-zero <zaerald.zd@gmail.com>
2017-06-09 21:12:04 +0800

    Refactor Time

--- app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
+++ app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
