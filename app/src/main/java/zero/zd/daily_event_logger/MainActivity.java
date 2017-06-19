dd970d1e64b9ced3bb821ceddba193f14c789ce3
zd-zero <zaerald.zd@gmail.com>
2017-06-19 12:50:05 +0800

    Change Sort Events from Latest to Old Events

--- app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
+++ app/src/main/java/zero/zd/daily_event_logger/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         Collections.sort(mEventList, new Comparator<Event>() {
             @Override
             public int compare(Event o1, Event o2) {
-                return o1.getDate().compareTo(o2.getDate());
+                return o2.getDate().compareTo(o1.getDate());
             }
         });
     }
