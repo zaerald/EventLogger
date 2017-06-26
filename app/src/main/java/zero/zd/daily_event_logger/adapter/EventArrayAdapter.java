40e4ba66799ff0788b6f753ba17b185e8d6c5411
zd-zero <zaerald.zd@gmail.com>
2017-06-27 05:40:47 +0800

    :sparkles: Time Relative Format
    :art: Reformat Code Structure
    
    Time Relaltive Format

--- app/src/main/java/zero/zd/daily_event_logger/adapter/EventArrayAdapter.java
+++ app/src/main/java/zero/zd/daily_event_logger/adapter/EventArrayAdapter.java
@@ -3,12 +3,15 @@ package zero.zd.daily_event_logger.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Date;
 import java.util.List;
 
 import zero.zd.daily_event_logger.Event;
@@ -59,11 +62,31 @@ public class EventArrayAdapter extends ArrayAdapter<Event> {
         Event event = mEventList.get(position);
 
         viewHolder.eventTextView.setText(event.getEvent());
-        viewHolder.dateTextView.setText(event.getStringDate());
+        viewHolder.dateTextView.setText(getStringDate(event));
 
         return convertView;
     }
 
+    private String getStringDate(Event event) {
+        long timeDifference = (new Date().getTime()) - event.getDate().getTime();
+        int s = (int) timeDifference / 1000;
+        int m = s / 60;
+        int h = m / 60;
+
+        if (h >= 24)
+            return event.getStringDate();
+        else if (h > 0)
+            return h + " " + getProperTimeForm("hr", h);
+        else if (m > 0)
+            return m + " " + getProperTimeForm("min", m);
+
+        return "a few seconds ago.";
+    }
+
+    private String getProperTimeForm(String noun, int value) {
+        if (value > 1) noun += "s";
+        return noun + " ago.";
+    }
 
     private static class ViewHolder {
         private TextView eventTextView;
