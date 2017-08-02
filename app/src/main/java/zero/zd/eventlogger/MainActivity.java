4082f4d8ac1e44bdbed971f39a6042dae707122a
zd-zero <zaerald.zd@gmail.com>
2017-08-02 21:56:33 +0800

    Handle Search Query Intent

--- app/src/main/java/zero/zd/eventlogger/MainActivity.java
+++ app/src/main/java/zero/zd/eventlogger/MainActivity.java
@@ -1,5 +1,6 @@
 package zero.zd.eventlogger;
 
+import android.app.SearchManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -54,11 +56,23 @@ public class MainActivity extends AppCompatActivity {
 
         initObjects();
         updateListView();
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
@@ -97,6 +111,13 @@ public class MainActivity extends AppCompatActivity {
                 showEventDialog();
             }
         });
+    }
+
+    private void handleIntent(Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_SEARCH)) {
+            String searchQuery = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, "Q: " + searchQuery, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void shareEvent(Event event) {
